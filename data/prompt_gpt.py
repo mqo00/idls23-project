8f8d95faa0545384297608bd07d5f506278e2421
Qianou Ma <qianoum@andrew.cmu.edu>
2023-05-01 00:34:52 -0400

    updated retriever & topk example selection

--- data/prompt_gpt.py
+++ data/prompt_gpt.py
@@ -70,7 +70,8 @@ class AskGPT():
         response["num_examples"] = n
         response["label"] = label
         response["prompt"] = messages
-        write_to_json(response, file='gpt_response.json')
+        response["temperature"] = self.temperature
+        write_to_json(response, file='gpt_retriever_response.json')
         return gpt_answer, messages
 
 
@@ -114,8 +115,76 @@ class EvalGPT(AskGPT):
                 time.sleep(2) # avoid rate limit
         print("All questions are evaluated!")
 
+# GPT = EvalGPT()
+# length = len(GPT.qa_pairs)
+# for n in [0, 1, 3, 5, 10, 15, 20]:
+#     GPT.evaluate_qas(start=0, end=length, num_examples=n)
+class TopKQA_DataLoader():
+    def __init__(self, qa_file='topk.json'):
+        self.qa_file = qa_file
+        self.qa_pairs = dict()
+    
+    def __len__(self):
+        if len(self.qa_pairs) == 0:
+            self.get_qa_pairs()
+        return len(self.qa_pairs)
 
-GPT = EvalGPT()
-length = len(GPT.qa_pairs)
-for n in [0, 1, 3, 5, 10, 15, 20]:
-    GPT.evaluate_qas(start=0, end=length, num_examples=n)
+    def get_qa_pairs(self):
+        with open(self.qa_file) as f:
+            json_list = json.load(f)[0]
+            print(len(json_list))
+            for d in json_list:
+                q = d["question"]
+                self.qa_pairs[q] = d
+        return self.qa_pairs
+    
+    def get_topk_qa_pair(self, q, k=1):
+        if len(self.qa_pairs) == 0:
+            self.get_qa_pairs()
+        d = self.qa_pairs[q]
+        return d["neighbors"][:k]
+
+class TopKGPT(AskGPT):
+    def __init__(self, gpt="gpt-3.5-turbo", temperature=0.2, qa_file='topk.json'):
+        super().__init__(gpt, temperature)
+        self.QA_data = TopKQA_DataLoader(qa_file)
+   
+    def get_qa_examples(self, q, n, reverse=False):
+        prompt = []
+        qas = self.QA_data.get_topk_qa_pair(q, n)
+        if reverse: qas = qas[::-1]
+        for (q, a) in qas:
+            qa = self.format_qa(q, a)
+            prompt += qa
+        return prompt
+
+    def evaluate_qas(self, n=1):
+        print(len(self.QA_data))
+        qas = list(self.QA_data.qa_pairs.values())
+        for i in tqdm(range(len(self.QA_data))):
+            d = qas[i]
+            q, a, label = d["question"], d["ta_answer"], d["label"]
+            self.examples = self.get_qa_examples(q, n)
+            gpt_answer = self.ask_gpt(q, n, label)[0]
+            data = {
+                "label": label,
+                "num_examples": n,
+                "temperature": self.temperature,
+                "question": q,
+                "ta_answer": a,
+                "gpt_answer": gpt_answer,
+            }
+            if "[OPENAI_ERROR]" in gpt_answer: data["error"] = True
+            # if n > 1:
+            #     self.examples = self.get_qa_examples(q, n, reverse=True)
+            #     gpt_answer_reversed_ex = self.ask_gpt(q, n, label)[0]
+            #     data["gpt_answer_reversed_ex"] = gpt_answer_reversed_ex
+            write_to_json(data, file='gpt_retriever_eval.json')
+            time.sleep(1) # avoid rate limit
+        print("All questions are evaluated!")
+
+
+for t in [0, 0.5, 0.8, 1.0]: # 0.2
+    GPT = TopKGPT(temperature=t, qa_file='topk-sample.json')
+    # for n in [1, 15]:
+    GPT.evaluate_qas(n=1)
